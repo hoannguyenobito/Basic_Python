4bdd65497f83cb31f66b1cd3b2e10da5f3990506
hoannguyenobito <dinhhoannguyen999@gmail.com>
2018-12-29 18:40:06 +0700

    First Python Program

---
+++ Test/test.py
@@ -0,0 +1,18 @@
+# gan bien a
+a = 4.88
+print(a)
+print(type(a))
+from decimal import*
+getcontext().prec = 30
+b = Decimal(10)/Decimal(3)
+print(b)
+# phan so
+from fractions import*
+frac1 = Fraction(6,9)
+frac2 = Fraction(5,7)
+print(frac1 + frac2)
+# so phuc
+c = complex(2,5)
+print(c)
+print(c.real)
+print(c.imag)
